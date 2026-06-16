529e8daf9ddc590430aaedc09f2faf2a442b58b0
crymall <reed.gaines@gmail.com>
2026-06-16 14:20:29 -0400

    cache invalidation

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -1,4 +1,7 @@
-export async function load({ fetch }) {
+export async function load({ fetch, setHeaders }) {
+	setHeaders({
+		'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0'
+	});
 	const response = await fetch('/api/posts');
 	const posts = await response.json();
 	return { posts };
